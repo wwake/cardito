b8a229503c81a11ecb0002c1fa5c7bf1ad3450bc
Bill Wake <william.wake@acm.org>
2022-09-06 22:10:01 -0500

    Initial checkin

---
+++ cardito/Shared/carditoApp.swift
@@ -0,0 +1,17 @@
+//
+//  carditoApp.swift
+//  Shared
+//
+//  Created by Bill Wake on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct carditoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
